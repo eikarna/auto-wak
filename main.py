670c26a2236acb3637d92f311ca968fa30b03a76
Eikarna <nixic0@proton.me>
2024-06-16 00:28:37 +0700

    Modified: main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def random_string(length):
     return ''.join([random.choice(string.ascii_letters + string.digits) for _ in range(length)])
 
 def generate_payload(namefile, eksfile, projectname, language):
-    entity = "{}.{}".format(random_string(8)) if namefile == "random" else namefile.format(eksfile)
+    entity = "{}.{}".format(random_string(8), eksfile) if namefile == "random" else "{}.{}".format(namefile, eksfile)
     project = random_string(12) if projectname == "random" else projectname
     
     return {
@@ -23,12 +23,16 @@ def generate_payload(namefile, eksfile, projectname, language):
         "lineno": str(random.randint(1, 9999)),
         "cursorpos": str(random.randint(0, 99999)),
         "lines": str(random.randint(1, 9999)),
+        "line_additions": str(random.randint(1, 9999)),
+        "line_deletions": str(random.randint(1, 9999)),
         "is_write": bool(random.getrandbits(1)),
-        "plugin": random.choice(["vscode", "pycharm", "sublime", "micro", "nano"]),
+        "plugin": random.choice(["vscode", "pycharm", "sublime", "nano", "micro", "jetbrains"]),
         "project": project,
         "language": language,
         "project_root_count": random.randint(1, 10),
-        "category": "coding"
+        "category": random.choice(["coding", "building", "indexing", "debugging", "browsing", "running tests", "writing tests", "manual testing", "writing docs", "communicating", "code reviewing", "researching", "learning", "designing"]),
+        "hostname": "Windows",
+        "branch": "main"
     }
 
 def main(apikey, namefile, eksfile, projectname, language):
@@ -38,11 +42,18 @@ def main(apikey, namefile, eksfile, projectname, language):
         "X-Machine-Name": platform.node()
     }
     
-    while True:
-        payload = generate_payload(namefile, eksfile, projectname, language)
-        response = requests.post(url, json=payload, headers=headers, params=querystring)
-        print(json.dumps(response.json(), indent=2))
-        time.sleep(1)
+    try:
+        while True:
+            try:
+                payload = generate_payload(namefile, eksfile, projectname, language)
+                response = requests.post(url, json=payload, headers=headers, params=querystring, timeout=1500)
+                print(json.dumps(response.json(), indent=2))
+                time.sleep(1)
+            except Exception as e:
+                print(f"An error occurred: {e}")
+                pass
+    except KeyboardInterrupt:
+        print("\nProgram interrupted by user. Exiting...")
 
 if __name__ == "__main__":
     if len(sys.argv) != 6:
