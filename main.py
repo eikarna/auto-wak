7abfe49c8d5656beac80e7409bc6f1043b44315b
Eikarna <nixic0@proton.me>
2024-06-15 17:29:30 +0700

    Modified: main.py

--- main.py
+++ main.py
@@ -4,13 +4,9 @@ from datetime import datetime, timedelta
 import random
 import string
 import json
+import sys
 
 url = "https://api.wakatime.com/api/v1/users/current/heartbeats"
-querystring = {"api_key": "YOUR_APIKEY"}
-headers = {
-    "content-type": "application/json",
-    "X-Machine-Name": "Windows"
-}
 
 def random_string(length):
     return ''.join([random.choice(string.ascii_letters + string.digits) for _ in range(length)])
@@ -22,23 +18,49 @@ def random_timestamp(max_days):
     random_time = now + timedelta(days=random_days, seconds=random_seconds)
     return int((random_time - datetime(1970, 1, 1)).total_seconds())
 
-def generate_payload():
+def generate_payload(namefile, projectname, language):
+    entity = "{}.go".format(random_string(8)) if namefile == "random" else namefile
+    project = random_string(12) if projectname == "random" else projectname
+    
     return {
         "type": "file",
-        "entity": "server.go", # "{}.go".format(random_string(8)),
-        "time": time.time(),
+        "entity": entity,
+        "time": random_timestamp(180),
         "lineno": str(random.randint(1, 9999)),
         "cursorpos": str(random.randint(0, 99999)),
         "lines": str(random.randint(1, 9999)),
         "is_write": bool(random.getrandbits(1)),
         "plugin": random.choice(["vscode", "pycharm", "sublime", "micro", "nano"]),
-        "project": "gotps-enet",
-        "language": "Go",
+        "project": project,
+        "language": language,
         "project_root_count": random.randint(1, 10),
         "category": "coding"
     }
 
-while True:
-    payload = generate_payload()
-    response = requests.post(url, json=payload, headers=headers, params=querystring)
-    print(json.dumps(response.json(), indent=2))
+def main(apikey, namefile, projectname, language):
+    querystring = {"api_key": apikey}
+    headers = {
+        "content-type": "application/json",
+        "X-Machine-Name": "Windows"
+    }
+    
+    while True:
+        payload = generate_payload(namefile, projectname, language)
+        response = requests.post(url, json=payload, headers=headers, params=querystring)
+        print(json.dumps(response.json(), indent=2))
+        time.sleep(1)
+
+if __name__ == "__main__":
+    if len(sys.argv) != 5:
+        print("Usage: python script.py <apikey> <namefile> <projectname> <language>")
+        sys.exit(1)
+    
+    apikey = sys.argv[1]
+    if len(apikey) < 1 or not "waka_" in apikey:
+        print("Invalid API Key!")
+        sys.exit(1)
+    namefile = sys.argv[2]
+    projectname = sys.argv[3]
+    language = sys.argv[4]
+    
+    main(apikey, namefile, projectname, language)
