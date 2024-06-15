30a32bf33e8a1588e0ef811e7d0d93ba889b7dd8
Eikarna <nixic0@proton.me>
2024-06-15 17:44:41 +0700

    Modified: main.py

--- main.py
+++ main.py
@@ -5,27 +5,21 @@ import random
 import string
 import json
 import sys
+import platform
 
 url = "https://api.wakatime.com/api/v1/users/current/heartbeats"
 
 def random_string(length):
     return ''.join([random.choice(string.ascii_letters + string.digits) for _ in range(length)])
 
-def random_timestamp(max_days):
-    now = datetime.now()
-    random_days = random.randint(0, max_days)
-    random_seconds = random.randint(0, 86400)  # Number of seconds in a day
-    random_time = now + timedelta(days=random_days, seconds=random_seconds)
-    return int((random_time - datetime(1970, 1, 1)).total_seconds())
-
-def generate_payload(namefile, projectname, language):
-    entity = "{}.go".format(random_string(8)) if namefile == "random" else namefile
+def generate_payload(namefile, eksfile, projectname, language):
+    entity = "{}.{}".format(random_string(8)) if namefile == "random" else namefile.format(eksfile)
     project = random_string(12) if projectname == "random" else projectname
     
     return {
         "type": "file",
         "entity": entity,
-        "time": random_timestamp(180),
+        "time": time.time(),
         "lineno": str(random.randint(1, 9999)),
         "cursorpos": str(random.randint(0, 99999)),
         "lines": str(random.randint(1, 9999)),
@@ -37,22 +31,22 @@ def generate_payload(namefile, projectname, language):
         "category": "coding"
     }
 
-def main(apikey, namefile, projectname, language):
+def main(apikey, namefile, eksfile, projectname, language):
     querystring = {"api_key": apikey}
     headers = {
-        "content-type": "application/json",
-        "X-Machine-Name": "Windows"
+        "Content-Type": "application/json",
+        "X-Machine-Name": platform.node()
     }
     
     while True:
-        payload = generate_payload(namefile, projectname, language)
+        payload = generate_payload(namefile, eksfile, projectname, language)
         response = requests.post(url, json=payload, headers=headers, params=querystring)
         print(json.dumps(response.json(), indent=2))
         time.sleep(1)
 
 if __name__ == "__main__":
-    if len(sys.argv) != 5:
-        print("Usage: python script.py <apikey> <namefile> <projectname> <language>")
+    if len(sys.argv) != 6:
+        print("Usage: python script.py <apikey> <namefile> <extensionfile> <projectname> <language>")
         sys.exit(1)
     
     apikey = sys.argv[1]
@@ -60,7 +54,8 @@ if __name__ == "__main__":
         print("Invalid API Key!")
         sys.exit(1)
     namefile = sys.argv[2]
-    projectname = sys.argv[3]
-    language = sys.argv[4]
+    eksfile = sys.argv[3]
+    projectname = sys.argv[4]
+    language = sys.argv[5]
     
-    main(apikey, namefile, projectname, language)
+    main(apikey, namefile, eksfile, projectname, language)
